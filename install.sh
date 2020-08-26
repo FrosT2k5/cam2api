3d5b741c0c97517608e97781ac9d4c1cfef44935
FrosT2k5 <androygaming@gmail.com>
2020-08-26 17:29:14 +0530

    Adding user permissions to script and more improvements Thanks to https://t.me/runrunrun00 for pointing out errors

--- install.sh
+++ install.sh
@@ -8,6 +8,9 @@ fi
 
 BINDIR="${PREFIX}/bin"
 CUD=`pwd`
+whoi=`ls -la /data/data/com.termux/files/usr/bin/pkg | cut -b 14-20`
+echo $whoi
+
 if [ -e $BINDIR/termux-info ]; then
 	OS=ANDROID
 else
@@ -19,23 +22,28 @@ if [ $OS = "ANDROID" ]; then
 	echo "Getting root access now"
 	su -c setprop camera.support 1
 	sleep 1
-	echo "Done"
+	echo "Done\n"
+	sleep 0.5
 
 	echo "Setting up files"
 	cp $CUD/files/gcamon $BINDIR/
 	cp $CUD/files/gcamoff $BINDIR/
 	sleep 0.5
-	echo "Done"
+	echo "Done\n"
+	sleep 0.5
 
 	echo "Setting up permissions"
-	chmod +x $BINDIR/gcamon
-	chmod +x $BINDIR/gcamoff
+	su -c chmod +x $BINDIR/gcamon
+	su -c chmod +x $BINDIR/gcamoff
+	su -c chown $whoi:$whoi $BINDIR/gcamon
+	su -c chown $whoi:$whoi $BINDIR/gcamoff
 	sleep 0.5
 	echo "Done"
-
-	echo "You are now ready to use the gcamoff/gcamon script"
+	sleep 0.5
+	
+	echo "You are now ready to use the gcamoff/gcamon script\n"
 	echo "To enable cam2api, enter gcamon"
-	echo "To disable cam2api, enter gcamoff"
+	echo "To disable cam2api, enter gcamoff\n"
 	echo "No need to reboot between enabling and disabling cam2api using this scripts"
 	echo "Script By Yash Patil\(FrosT\) Have fun :\)"
 
